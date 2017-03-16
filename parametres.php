584b39699e27ad2213ea3bd2e0e59d8fcf2ca3a2
PixOFHeaven <pixofheaven@gmail.com>
2017-03-16 23:39:13 +0100

    Ajout du fichier parametres du projet

---
+++ parametres.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require("librairie/Database.class.php");
+require("librairie/RessourcesBAKE.class.php");
+require("php/datas.php");
+require("php/admin.php");
+
+define("URL",$_SERVER["REQUEST_URI"]);
+define("URL_CREA","../../Projets/");
